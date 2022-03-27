6b79c4f0933ee75d24c7f5020e71acdc94647b80
Mr.Corvo <76906260+Mr-Corvo@users.noreply.github.com>
2022-03-27 06:09:07 +0400

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -1,20 +1,25 @@
 var eventsKeys = [
    {
       id: 'now',
-      title: `Perfect Days in a row(${getDifferenceDate("2022-03-19",dateEndEvent)})`,
-      start: "2022-03-19",
+      title: `Perfect Days in a row(${getDifferenceDate("2022-03-28",dateEndEvent)})`,
+      start: "2022-03-28",
       end: dateEndEvent,
       classNames: ['_event-now']
    },
    {
+      title: `Perfect Days in a row(${getDifferenceDate("2022-03-19","2022-03-27")})`,
+      start: "2022-03-19",
+      end: "2022-03-27"
+   },
+   {
       title: `Perfect Days in a row(${getDifferenceDate("2022-03-14","2022-03-19")})`,
       start: "2022-03-14",
-      end: "2022-03-19",
+      end: "2022-03-19"
    },
    {
       title: `Perfect Days in a row(${getDifferenceDate("2022-02-21","2022-03-14")})`,
       start: "2022-02-21",
-      end: "2022-03-14",
+      end: "2022-03-14"
    },
    {
       title: `Perfect Days in a row(${getDifferenceDate("2022-02-11","2022-02-20")})`,
@@ -24,36 +29,36 @@ var eventsKeys = [
    {
       title: `Perfect Days in a row(${getDifferenceDate("2022-02-07","2022-02-10")})`,
       start: "2022-02-07",
-      end: "2022-02-10",
+      end: "2022-02-10"
    },
    {
       title: `Perfect Days in a row(${getDifferenceDate("2022-01-24","2022-02-06")})`,
       start: "2022-01-24",
-      end: "2022-02-06",
+      end: "2022-02-06"
    },
    { 
       title: `Perfect Days in a row(${getDifferenceDate("2022-01-01","2022-01-23")})`,
       start: "2022-01-01",
-      end: "2022-01-23",
+      end: "2022-01-23"
    },
    { 
       title: `Perfect Days in a row(${getDifferenceDate("2021-11-22","2021-12-29")})`,
       start: "2021-11-22",
-      end: "2021-12-29",
+      end: "2021-12-29"
    },
    {
       title: `Perfect Days in a row(${getDifferenceDate("2021-11-15","2021-11-21")})`,
       start: "2021-11-15",
-      end: "2021-11-21",
+      end: "2021-11-21"
    },
    {
       title: `Perfect Days in a row (${getDifferenceDate("2021-10-11","2021-11-14")})`,
       start: "2021-10-11",
-      end: "2021-11-14",
+      end: "2021-11-14"
    },
    {
       title: `Perfect Days in a row (${getDifferenceDate("2021-10-04","2021-10-10")})`,
       start: "2021-10-04",
-      end: "2021-10-10",
+      end: "2021-10-10"
    },
 ];
