abe7fd1670f5930b16a20722fa57437b840c1caf
Mr.Corvo <76906260+Mr-Corvo@users.noreply.github.com>
2022-06-27 06:29:16 +0400

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -1,8 +1,8 @@
 var eventsKeys = [
    {
       id: 'now',
-      title: `Perfect Days in a row(${getDifferenceDate("2022-06-20",dateEndEvent)})`,
-      start: "2022-06-20",
+      title: `Perfect Days in a row(${getDifferenceDate("2022-06-27",dateEndEvent)})`,
+      start: "2022-06-27",
       end: dateEndEvent,
       classNames: ['_event-now']
    },
